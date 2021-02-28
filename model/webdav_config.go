14fdb26de65f0f9f2e02b4ccbbca56641a2f3c88
OnceAce <71519009+OnceAce@users.noreply.github.com>
2021-02-28 17:15:41 +0800

    Update webdav_config.go

--- model/webdav_config.go
+++ model/webdav_config.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"strings"
 )
-
 type WebDAVConfig struct {
 	Prefix   string
 	PathDir  string
@@ -15,32 +14,24 @@ type WebDAVConfig struct {
 	Handler  *webdav.Handler
 }
 
-func (WebDAVConfig *WebDAVConfig) Init(prefix string, pathDir string, username string, password string, readonly bool) {
-	WebDAVConfig.Prefix = prefix
-	WebDAVConfig.PathDir = pathDir
-	WebDAVConfig.Username = username
-	WebDAVConfig.Password = password
-	WebDAVConfig.ReadOnly = readonly
-
-	WebDAVConfig.Handler = &webdav.Handler{
-		FileSystem: webdav.Dir(pathDir),
-		LockSystem: webdav.NewMemLS(),
-		Prefix:     prefix,
-	}
-}
 func (WebDAVConfig *WebDAVConfig) InitByConfigStr(str string) {
 	davConfigArray := strings.Split(str, ",")
-	prefix := davConfigArray[0]
-	pathDir := davConfigArray[1]
-	username := davConfigArray[2]
-	password := davConfigArray[3]
+	WebDAVConfig.Prefix= davConfigArray[0]
+	WebDAVConfig.PathDir = davConfigArray[1]
+	WebDAVConfig.Username = davConfigArray[2]
+	WebDAVConfig.Password = davConfigArray[3]
 
 	readonly, err := strconv.ParseBool(davConfigArray[4])
 	if err != nil {
 		readonly = false
 	}
+	WebDAVConfig.ReadOnly = readonly
 
-	WebDAVConfig.Init(prefix, pathDir, username, password, readonly)
+	WebDAVConfig.Handler = &webdav.Handler{
+		FileSystem: webdav.Dir(davConfigArray[1]),
+		LockSystem: webdav.NewMemLS(),
+		Prefix:     davConfigArray[0],
+	}
 }
 func WebDAVConfigFindOneByPrefix(WebDAVConfigs []*WebDAVConfig, prefix string) *WebDAVConfig {
 	for _, WebDAVConfig := range WebDAVConfigs {
